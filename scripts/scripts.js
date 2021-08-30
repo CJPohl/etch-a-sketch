fb7ccc3e320a5c49e3804a90e0562ae93989d229
CJPohl <cjpohldev@gmail.com>
2021-08-30 18:47:06 -0400

    Major changes to index.html and styles.css

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,12 +1,14 @@
 const container = document.querySelector('.container'); // main container
-const divList = []; // node list
+const divList = []; // div git node list
 function createBoxes () { // function to create dom boxes
     for (let i=0; i<16; i++) {
         divList[i] = document.createElement('div');
         container.appendChild(divList[i]);  
     }
+    divList.forEach(divList => divList.classList.add('item')); // add styling class to DOM boxes
 }
 
 
-createBoxes();
-divList.forEach(divList => divList.classList.add('item')); // add styling class to DOM boxes
+createBo
+
+
